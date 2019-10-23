6f14725c9a13e1c2e0dd0b287f37346b2f670c3c
Doom <lightfollower1985@gmail.com>
2019-10-23 15:24:38 +0500

    фыв

--- toDo/src/main/java/toDo/persist/ToDoRepository.java
+++ toDo/src/main/java/toDo/persist/ToDoRepository.java
@@ -1,16 +1,39 @@
 package toDo.persist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+@ApplicationScoped
+@Named
 public class ToDoRepository {
+    private static final Logger logger = LoggerFactory.getLogger(ToDoRepository.class);
 
-    public final Connection conn;
+    @Inject
+    private DataSource dataSource;
 
-    public ToDoRepository(Connection conn) throws SQLException {
-        this.conn = conn;
+    private Connection conn;
+
+    @PostConstruct
+    public void init() throws SQLException {
+        this.conn = dataSource.getConnection();
+
+        if (this.findAll().isEmpty()) {
+            this.insert(new ToDo(-1L, "First", LocalDate.now()));
+            this.insert(new ToDo(-1L, "Second", LocalDate.now().plusDays(1)));
+            this.insert(new ToDo(-1L, "Third", LocalDate.now().plusDays(2)));
+        }
+
         createTableIfNotExists(conn);
     }
 
@@ -20,7 +43,6 @@ public class ToDoRepository {
             stmt.setString(1, toDo.getDescription());
             stmt.setDate(2, Date.valueOf(toDo.getTargetDate()), Calendar.getInstance());
             stmt.execute();
-
         }
     }
 
@@ -38,7 +60,6 @@ public class ToDoRepository {
         try (PreparedStatement stmt = conn.prepareStatement(
                 "delete from todos where id = ?;")) {
             stmt.setLong(1, id);
-
             stmt.execute();
         }
     }
@@ -61,7 +82,6 @@ public class ToDoRepository {
         try (Statement stmt = conn.createStatement()) {
             ResultSet rs = stmt.executeQuery("select id, description, targetDate from todos");
 
-
             while (rs.next()) {
                 res.add(new ToDo(rs.getLong(1), rs.getString(2), rs.getDate(3, Calendar.getInstance()).toLocalDate()));
             }
@@ -72,7 +92,7 @@ public class ToDoRepository {
     private void createTableIfNotExists(Connection conn) throws SQLException {
         try (Statement stmt = conn.createStatement()) {
             stmt.execute("create table if not exists todos (\n" +
-                    "\t id int auto_increment primary key,\n" +
+                    "\tid int auto_increment primary key,\n" +
                     "    description varchar(25),\n" +
                     "    targetDate date \n" +
                     ");");
