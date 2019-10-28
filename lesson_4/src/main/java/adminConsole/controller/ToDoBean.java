806351aaf5ea4af2ba3759f641f6e6d96a025c83
Doom <lightfollower1985@gmail.com>
2019-10-28 16:50:16 +0500

    Фыв

---
+++ lesson_4/src/main/java/adminConsole/controller/ToDoBean.java
@@ -0,0 +1,67 @@
+package adminConsole.controller;
+
+import adminConsole.persist.ToDo;
+import adminConsole.persist.ToDoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@SessionScoped
+@Named
+public class ToDoBean implements Serializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(ToDoBean.class);
+
+    @Inject
+    private ToDoRepository toDoRepository;
+
+    private ToDo toDo;
+
+    private List<ToDo> toDoList;
+
+    public void preloadTodoList(ComponentSystemEvent componentSystemEvent) {
+        this.toDoList = toDoRepository.findAll();
+    }
+
+    public ToDo getToDo() {
+        return toDo;
+    }
+
+    public void setToDo(ToDo toDo) {
+        this.toDo = toDo;
+    }
+
+    public List<ToDo> getAllTodo() {
+        return toDoList;
+    }
+
+    public String createTodo() {
+        this.toDo = new ToDo();
+        return "/todo.xhtml?faces-redirect=true";
+    }
+
+    public String saveTodo() {
+        if (toDo.getId() == null) {
+            toDoRepository.insert(toDo);
+        } else {
+            toDoRepository.update(toDo);
+        }
+        return "/index.xhtml?faces-redirect=true";
+    }
+
+    public void deleteTodo(ToDo toDo) {
+        logger.info("Deleting ToDo.");
+        toDoRepository.delete(toDo.getId());
+    }
+
+    public String editTodo(ToDo toDo) {
+        this.toDo = toDo;
+        return "/todo.xhtml?faces-redirect=true";
+    }
+}
